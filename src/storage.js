90134f2e63a5234869e175a425a2e66b76725f10
Harty <130511091+HartyDITF@users.noreply.github.com>
2026-07-28 02:27:51 +0300

    Update storage.js

--- src/storage.js
+++ src/storage.js
@@ -1,1 +1,88 @@
+import fs from "fs";
 
+
+const FILE =
+"./sent.json";
+
+
+
+function loadStorage(){
+
+    if(
+        !fs.existsSync(FILE)
+    ){
+
+        return {};
+
+    }
+
+
+    try {
+
+        return JSON.parse(
+            fs.readFileSync(
+                FILE,
+                "utf8"
+            )
+        );
+
+
+    } catch {
+
+        return {};
+
+    }
+
+}
+
+
+
+
+function saveStorage(data){
+
+    fs.writeFileSync(
+        FILE,
+        JSON.stringify(
+            data,
+            null,
+            2
+        )
+    );
+
+}
+
+
+
+
+export function wasSent(id){
+
+
+    const data =
+        loadStorage();
+
+
+    return Boolean(
+        data[id]
+    );
+
+}
+
+
+
+
+export function markSent(id){
+
+
+    const data =
+        loadStorage();
+
+
+    data[id] =
+        Date.now();
+
+
+    saveStorage(
+        data
+    );
+
+}
