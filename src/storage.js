9c78ac8e93e9a79ad48d99c2c98f5ea68d7952f6
Harty <130511091+HartyDITF@users.noreply.github.com>
2026-07-28 05:48:52 +0300

    Update storage.js

--- src/storage.js
+++ src/storage.js
@@ -1,73 +1,33 @@
 import fs from "fs";
 
+const FILE = "./sent.json";
 
-const FILE="./sent.json";
+function load() {
+    if (!fs.existsSync(FILE)) return {};
 
-
-
-function load(){
-
-if(!fs.existsSync(FILE))
-return {};
-
-
-try{
-
-return JSON.parse(
-fs.readFileSync(FILE,"utf8")
-);
-
-}
-catch{
-
-return {};
-
+    try {
+        return JSON.parse(fs.readFileSync(FILE, "utf8"));
+    } catch {
+        return {};
+    }
 }
 
+function save(data) {
+    fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
 }
 
-
-
-function save(data){
-
-fs.writeFileSync(
-FILE,
-JSON.stringify(
-data,
-null,
-2
-)
-);
-
+export function getLastEpisode(id) {
+    const data = load();
+    return data[id] || 0;
 }
 
-
-
-export function getLastEpisode(id){
-
-const data=load();
-
-return data[id] || 0;
-
+export function updateEpisode(id, episode) {
+    const data = load();
+    data[id] = episode;
+    save(data);
 }
 
-
-
-export function updateEpisode(id,episode){
-
-const data=load();
-
-
-data[id]=episode;
-
-
-save(data);
-
-
-console.log(
-"💾 Сохранено:",
-id,
-episode
-);
-
+export function hasData() {
+    const data = load();
+    return Object.keys(data).length > 0;
 }
